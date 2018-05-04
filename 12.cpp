ffabc582be471e455386a8efd60b66152cdce3a6
Lukas <35837307+lukastatarunas@users.noreply.github.com>
2018-05-04 15:24:04 +0300

    Create 12.cpp

---
+++ 12.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int matrix[10][10], n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cin >> matrix[i][j];
+		}
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = n - 1; j >= 0; j--)
+		{
+			if (j == n - n)
+			{
+				cout << matrix[j][i];
+			}
+			else
+			{
+				cout << matrix[j][i] << " ";
+			}
+		}
+		cout << endl;
+	}
+}
