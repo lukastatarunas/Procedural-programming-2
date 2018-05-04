f90a383b5d399bda1bbba105d6977602d1a73853
Lukas <35837307+lukastatarunas@users.noreply.github.com>
2018-05-04 15:19:14 +0300

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int matrix[10], n, suma = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> matrix[i];
+		suma = suma + matrix[i];
+	}
+	cout << suma;
+}
