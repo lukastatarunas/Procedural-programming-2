c85e5e4497ba3d27dae34e95a47d9ed9061c697e
Lukas <35837307+lukastatarunas@users.noreply.github.com>
2018-05-04 10:20:25 +0300

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	int n, matrix[10];
+	double sum = 0, a = 0, c = 0;
+	cin >> n;
+	if (n >= 1 && n <= 10)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			cin >> matrix[i];
+			if (matrix[i] < 0)
+			{
+				sum = sum + matrix[i];
+				c++;
+			}
+		}
+		a = sum / c;
+		if (c > 0)
+		{
+			cout << fixed << setprecision(2) << a;
+		}
+		else if (c == 0)
+		{
+			cout << "NO";
+		}
+	}
+}
