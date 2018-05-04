961abcad63b1766231189145bcb1e5989c07f807
Lukas <35837307+lukastatarunas@users.noreply.github.com>
2018-05-04 15:23:45 +0300

    Create 11.cpp

---
+++ 11.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n = 0;
+	cin >> n;
+	n = n / 2;
+	for (int i =- n; i <= n; i++)
+	{
+		for (int j =- n; j <= n; j++)
+		{
+			if (abs(i) + abs(j) == n)
+			{
+				cout << "O";
+			}
+			else
+			{
+				cout << "-";
+			}
+		}
+		cout << endl;
+	}
+}
