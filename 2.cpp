070c9acc5f8f42ed3d3c996bf3fd1ef06175601b
Lukas <35837307+lukastatarunas@users.noreply.github.com>
2018-06-14 10:49:52 +0300

    2.cpp

--- 2.cpp
+++ 2.cpp
@@ -8,25 +8,22 @@ int main()
 	int n, matrix[10];
 	double sum = 0, a = 0, c = 0;
 	cin >> n;
-	if (n >= 1 && n <= 10)
+	for (int i = 0; i < n; i++)
 	{
-		for (int i = 0; i < n; i++)
+		cin >> matrix[i];
+		if (matrix[i] < 0)
 		{
-			cin >> matrix[i];
-			if (matrix[i] < 0)
-			{
-				sum = sum + matrix[i];
-				c++;
-			}
-		}
-		a = sum / c;
-		if (c > 0)
-		{
-			cout << fixed << setprecision(2) << a;
-		}
-		else if (c == 0)
-		{
-			cout << "NO";
+			sum = sum + matrix[i];
+			c++;
 		}
 	}
-}.
+	a = sum / c;
+	if (c > 0)
+	{
+		cout << fixed << setprecision(2) << a;
+	}
+	else if (c == 0)
+	{
+		cout << "NO";
+	}
+}
